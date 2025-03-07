tq-systems/u-boot-tqmaxx

clk: clk-rcar-gen3: Add parent clocks of watchdog and calculate clock rates
This patch adds parent clocks of watchdog used in different SoCs
(r8a774c0, r8a774a1, r8a774b1):
- CLK_TYPE_GEN3_R
- CLK_TYPE_GEN3_OSC
- CLK_TYPE_FR
- CLK_TYPE_DIV6_RO
- CLK_TYPE_GEN3_RCKSEL

Signed-off-by: Khoi Nguyen <khoi.nguyen.aj@renesas.com>
Signed-off-by: hienhuynh <hien.huynh.px@renesas.com>

drivers/clk/renesas/clk-rcar-gen3.c
@@ -25,6 +25,8 @@
 #include "renesas-cpg-mssr.h"
 #include "rcar-gen3-cpg.h"
 
+#define CPG_RCKCR_CKSEL		BIT(15) /* RCLK Clock Source Select */
+
 #define CPG_PLL0CR		0x00d8
 #define CPG_PLL2CR		0x002c
 #define CPG_PLL4CR		0x01f4
@@ -186,6 +188,7 @@ static u64 gen3_clk_get_rate64(struct clk *clk)
 	const struct rcar_gen3_cpg_pll_config *pll_config =
 					priv->cpg_pll_config;
 	u32 value, div, prediv, postdiv;
+	unsigned int rckcr_div;
 	u64 rate = 0;
 	int i, ret;
 
@@ -343,6 +346,43 @@ static u64 gen3_clk_get_rate64(struct clk *clk)
 
 		return -EINVAL;
 
+	case CLK_TYPE_FR:
+		rate = core->mult;
+		return rate;
+
+	case CLK_TYPE_DIV6_RO:
+		div = core->div;
+		rckcr_div = (readl(priv->base + core->offset) & 0x3f);
+		if (rckcr_div != 0x2f) {
+			writel(readl(priv->base + core->offset) | BIT(8),
+			       priv->base + core->offset);
+			udelay(105);
+			writel(((readl(priv->base + core->offset) & (BIT(15) |
+				 BIT(12))) | 0x2f), priv->base + core->offset);
+			rckcr_div = (readl(priv->base + core->offset) & 0x3f);
+		}
+		div *= (readl(priv->base + core->offset) & 0x3f) + 1;
+		rate = gen3_clk_get_rate64(&parent) / div;
+		return rate;
+
+	case CLK_TYPE_GEN3_RCKSEL:
+		if (readl(priv->base + CPG_RCKCR) & CPG_RCKCR_CKSEL) {
+			div = core->div & 0xffff;
+		} else {
+			parent.id = parent.id >> 16;
+			div = core->div >> 16;
+		}
+		rate = gen3_clk_get_rate64(&parent) / div;
+		return rate;
+
+	case CLK_TYPE_GEN3_R:
+		rate = gen3_clk_get_rate64(&parent);
+		return rate;
+
+	case CLK_TYPE_GEN3_OSC:
+		div = pll_config->osc_prediv * core->div;
+		rate = gen3_clk_get_rate64(&parent) / div;
+		return rate;
 	}
 
 	printf("%s[%i] unknown fail\n", __func__, __LINE__);

bugraaydogar/renesas-u-boot-cip

drivers/clk/renesas/clk-rcar-gen3.c
@@ -30,6 +30,8 @@
 #include "rcar-gen3-cpg.h"
 #include "rcar-cpg-lib.h"
 
+#define CPG_RCKCR_CKSEL		BIT(15) /* RCLK Clock Source Select */
+
 #define CPG_PLL0CR		0x00d8
 #define CPG_PLL2CR		0x002c
 #define CPG_PLL4CR		0x01f4
@@ -191,6 +193,7 @@ static u64 gen3_clk_get_rate64(struct clk *clk)
 	const struct rcar_gen4_cpg_pll_config *gen4_pll_config =
 					priv->gen4_cpg_pll_config;
 	u32 value, div;
+	unsigned int rckcr_div;
 	u64 rate = 0;
 	u8 shift;
 	int ret;
@@ -419,6 +422,43 @@ static u64 gen3_clk_get_rate64(struct clk *clk)
 		return rcar_clk_get_rate64_rpcd2(core->parent,
 						 gen3_clk_get_rate64(&parent));
 
+	case CLK_TYPE_FR:
+		rate = core->mult;
+		return rate;
+
+	case CLK_TYPE_DIV6_RO:
+		div = core->div;
+		rckcr_div = (readl(priv->base + core->offset) & 0x3f);
+		if (rckcr_div != 0x2f) {
+			writel(readl(priv->base + core->offset) | BIT(8),
+			       priv->base + core->offset);
+			udelay(105);
+			writel(((readl(priv->base + core->offset) & (BIT(15) |
+				 BIT(12))) | 0x2f), priv->base + core->offset);
+			rckcr_div = (readl(priv->base + core->offset) & 0x3f);
+		}
+		div *= (readl(priv->base + core->offset) & 0x3f) + 1;
+		rate = gen3_clk_get_rate64(&parent) / div;
+		return rate;
+
+	case CLK_TYPE_GEN3_RCKSEL:
+		if (readl(priv->base + CPG_RCKCR) & CPG_RCKCR_CKSEL) {
+			div = core->div & 0xffff;
+		} else {
+			parent.id = parent.id >> 16;
+			div = core->div >> 16;
+		}
+		rate = gen3_clk_get_rate64(&parent) / div;
+		return rate;
+
+	case CLK_TYPE_GEN3_R:
+		rate = gen3_clk_get_rate64(&parent);
+		return rate;
+
+	case CLK_TYPE_GEN3_OSC:
+		div = gen3_pll_config->osc_prediv * core->div;
+		rate = gen3_clk_get_rate64(&parent) / div;
+		return rate;
 	}
 
 	printf("%s[%i] unknown fail\n", __func__, __LINE__);